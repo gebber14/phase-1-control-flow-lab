12b42c6b08c7e443a68d2c65a90bff2667070449
Jason Rothgeb <jrothgeb@gmail.com>
2022-06-21 09:04:01 -0500

    commit tree d168e10906f0b4cd5f2f04f978ff7ba7e824c304

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber) {
+  let result
+  if (someNumber <= 400) {
+    result = "This one is on me!";
+  } else if (someNumber <= 2000) {
+    result = "That will be twenty bucks."
+  } else if (someNumber < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = "No can do."
+  }
+  return result; 
+}  
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+function ternaryCheckCity(city){
   // Write your code here!
+  switch(city) {
+    case 'NYC':
+      return "Ok, sounds good.";
+      break;
+    default:
+      return "No go.";
+      break;
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
